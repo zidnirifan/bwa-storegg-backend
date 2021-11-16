8aac31f612bdf5ce0e65e4bc18c5b6d12a66eaea
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-11-16 13:48:08 +0700

    delete nominal

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -68,20 +68,20 @@ module.exports = {
       res.redirect('/nominal');
     }
   },
-  // actionDelete: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-  //     await Category.findOneAndRemove({ _id: id });
+      await Nominal.findOneAndRemove({ _id: id });
 
-  //     req.flash('alertMessage', 'Berhasil hapus kategory');
-  //     req.flash('alertStatus', 'success');
+      req.flash('alertMessage', 'Berhasil hapus nominal');
+      req.flash('alertStatus', 'success');
 
-  //     res.redirect('/category');
-  //   } catch (err) {
-  //     req.flash('alertMessage', err.message);
-  //     req.flash('alertStatus', 'danger');
-  //     res.redirect('/category');
-  //   }
-  // },
+      res.redirect('/nominal');
+    } catch (err) {
+      req.flash('alertMessage', err.message);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/nominal');
+    }
+  },
 };
