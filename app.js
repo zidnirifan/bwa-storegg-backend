3dcae4b94b3216dd9e5c18c4c3a79509da695824
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-12-28 16:39:52 +0700

    implement cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 const dashboardRouter = require('./app/dashboard/router');
 const categoryRouter = require('./app/category/router');
@@ -21,6 +22,7 @@ const authRouter = require('./app/auth/router');
 const app = express();
 const apiPath = '/api/v1';
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
